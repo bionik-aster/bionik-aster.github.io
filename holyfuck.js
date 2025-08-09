4114b38a47782e83871df2fd204a501026709a32
Aster Irving <bion1kbak0n@gmail.com>
2025-08-09 12:57:21 +0100

    Update holyfuck.js

--- holyfuck.js
+++ holyfuck.js
@@ -94,6 +94,7 @@ function cockandballs() {
         `<span style="color:#00bbff;">listen kid. aster, they're hiding something. i need to tell you before i get sho-<br/> -a dead kommit</span>`,
         `<span style="color:#00ff00;">i have thirty babies<br/> -echo, still high on weed</span>`,
         `<span style="color:#ffcd9c;">when the dust settles, just you and me remain.</span>`,
+        `<span style="color:#ffcd9c;">https://files.catbox.moe/jbfwh6.jpg</span>`,
 
 
     ];
