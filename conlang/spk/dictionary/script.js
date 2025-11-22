e7830b64522cba0cece73edf15b01c59d38d9cc2
Aster Irving <bion1kbak0n@gmail.com>
2025-11-22 10:45:02 +0000

    Update script.js

--- conlang/spk/dictionary/script.js
+++ conlang/spk/dictionary/script.js
@@ -37,8 +37,8 @@ function sdimbt() {
         <div class="wordbox-content">
           <h2>${wordData.word}</h2>
           <p class="wordclass">${wordData.wordclass}</p>
-          <p><b>Alternate animate forms:</b> ${wordData.forms}</p>
-          <p><b>Alternate inanimate forms:</b> ${wordData.inanforms}</p>
+          <p><b>Alternate animate forms:</b> ${wordData.forms || "None"}</p>
+          <p><b>Alternate inanimate forms:</b> ${wordData.inanforms || "None"}</p>
           <p><b>Definition:</b> ${wordData.def}</p>
           <p><b>Usage in a sentence:</b> ${wordData.example}</p>
           <p><b>Translation:</b> ${wordData.exampletrans}</p>
